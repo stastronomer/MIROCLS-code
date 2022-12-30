cf4d8ce91a6fb8d56412a95602fa04ac847dc606
Akash Gupta <60941175+stastronomer@users.noreply.github.com>
2022-12-30 18:25:46 +0100

    update 1

--- test.py
+++
@@ -1,5 +0,0 @@
-import test_class
-
-import cluster_searcher_2
-
-cluster_searcher_2.search()
--- test_class.py
+++
@@ -1,8 +0,0 @@
-class test:
-    def __init__(self, a):
-        self.a = a
-        print('rhis works')
-    def ass(self):
-        print(self.a)
-
-
